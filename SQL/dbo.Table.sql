fd0cc90d7fbf944f82f38e94570b0a4ef32d4e44
ShayanBamyar <shayanbamyar97@gmail.com>
2023-10-27 19:41:20 +0330

    project init

---
+++ SQL/dbo.Table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[EmployeeSalaryPayment_Tbl]
+(
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+    [FirstName] NVARCHAR(150) NOT NULL,
+    [LastName] NVARCHAR(150) NOT NULL,
+    [BasicSalary] DECIMAL(15) NOT NULL,
+    [Allowance] DECIMAL(15) NOT NULL,
+    [Transportation] DECIMAL(15) NOT NULL,
+    [Date] DATETIME NOT NULL
+);
